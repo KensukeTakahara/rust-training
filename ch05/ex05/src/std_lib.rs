560cfdc8467c7f0701029ab2c6c0f072ee02707a
Kensuke Takahara <zawcjk@gmail.com>
2020-06-22 00:06:33 +0900

    practice result.

--- ch05/ex05/src/std_lib.rs
+++ ch05/ex05/src/std_lib.rs
@@ -200,6 +200,30 @@ fn check14() {
   assert_eq!(add_elems(&[7, 11]), None);
 }
 
+fn check15() {
+  assert_eq!("10".parse::<i32>(), Ok(10));
+  let res0 = "a".parse::<i32>();
+  assert!(res0.is_err());
+  println!("{:?}", res0);
+}
+
+fn add0(s0: &str, s1: &str) -> Result<i32, std::num::ParseIntError> {
+  let s0 = s0.parse::<i32>()?;
+  let s1 = s1.parse::<i32>()?;
+  Ok(s0 + s1)
+}
+
+fn check16() {
+  assert_eq!(add0("3", "127"), Ok(3 + 127));
+  assert!(add0("3", "abc").is_err());
+}
+
+fn add1(s0: &str, s1: &str) -> Result<i32, String> {
+  let s0 = s0.parse::<i32>().map_err(|_e| "s0が整数ではありません")?;
+  let s1 = s1.parse::<i32>().map_err(|_e| "s1が整数ではありません")?;
+  Ok(s0 + s1)
+}
+
 pub fn check() {
   check1();
   check2();
@@ -216,4 +240,7 @@ pub fn check() {
   check12();
   check13();
   check14();
+  check15();
+  check16();
+  assert_eq!(add1("3", "abc"), Err("s1が整数ではありません".to_string()));
 }
