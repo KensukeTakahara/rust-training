4d40b5f960d9ed64712d3f856d098dce3cfc459d
Kensuke Takahara <zawcjk@gmail.com>
2020-07-12 12:43:48 +0900

    practice enum.

--- ch05/ex05/src/new_type.rs
+++ ch05/ex05/src/new_type.rs
@@ -110,6 +110,74 @@ fn check6() {
   assert_eq!(uv1, uv2);
 }
 
+#[derive(Debug, PartialEq)]
+enum Weekday {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+}
+
+fn say_something(weekday: Weekday) {
+  if weekday == Weekday::Friday {
+    println!("TGIF!");
+  } else {
+    println!("まだ{:?}か", weekday);
+  }
+}
+
+fn check7() {
+  say_something(Weekday::Friday);
+  say_something(Weekday::Monday);
+  say_something(Weekday::Tuesday);
+  say_something(Weekday::Wednesday);
+  say_something(Weekday::Thursday);
+}
+
+#[derive(Debug)]
+enum Task {
+  Open,
+  AssignedTo(UserName),
+  Working {
+    assignee: UserName,
+    remaining_hours: u16,
+  },
+  Done,
+}
+
+fn check8() {
+  let tasks = vec![
+    Task::Open,
+    Task::AssignedTo(String::from("junko")),
+    Task::Working {
+      assignee: String::from("hiro"),
+      remaining_hours: 18,
+    },
+    Task::Done,
+  ];
+
+  for (i, task) in tasks.iter().enumerate() {
+    match task {
+      Task::AssignedTo(assignee) => {
+        println!("タスク{}は{}さんにアサインされています。", i, assignee);
+      }
+      Task::Working {
+        assignee,
+        remaining_hours,
+      } => {
+        println!(
+          "タスク{}は{}さんが作業中です。残り{}時間の見込み",
+          i, assignee, remaining_hours
+        );
+      }
+      _ => {
+        println!("タスク{}はその他のステータス({:?})です。", i, task);
+      }
+    }
+  }
+}
+
 pub fn check() {
   check1();
   check2();
@@ -117,4 +185,6 @@ pub fn check() {
   check4();
   check5();
   check6();
+  check7();
+  check8();
 }
